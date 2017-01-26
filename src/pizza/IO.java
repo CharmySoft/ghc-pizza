afadbbdfcb2faf0cf8d47e2cade80a8ae83a08aa
CharmySoft <charmysoft@gmail.com>
2017-01-26 01:38:06 +0000

    Refine IO

--- src/pizza/IO.java
+++ src/pizza/IO.java
@@ -9,24 +9,26 @@ public class IO {
 	public boolean read(String filename) throws FileNotFoundException
 	{
 		Scanner in = new Scanner(new File(filename)); 
+		Pizza pizza;
 		try{
 			String[] headInfo = in.nextLine().split(" ");
 			int[] head = Arrays.stream(headInfo).mapToInt(Integer::parseInt).toArray();
-			Pizza pizza = new Pizza(head[0], head[1], head[2], head[3]);
+			pizza = new Pizza(head[0], head[1], head[2], head[3]);
 			for (int i = 0; i < pizza.getRow(); i++) {
 				String line = in.nextLine();
 				for (int j = 0; j < pizza.getColumn(); j++) {
 					pizza.setCell(j, i, line.charAt(j));
 				}
 			}
-			pizza.print();
-			pizza.cut();
 		} catch (Exception e){
-			e.printStackTrace();
+			System.out.println("Invalid Input file.");
 			return false;
 		} finally {
 			in.close();
 		}
+
+		pizza.print();
+		pizza.cut();
 		return true;
 	}
 }
